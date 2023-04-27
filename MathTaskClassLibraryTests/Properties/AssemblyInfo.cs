e448b2f01b519e2264da0cae79920151db00bc45
SashaVoin5 <sasha04ru@mail.ru>
2023-04-27 21:42:53 +0700

    Добавьте файлы проекта.

---
+++ MathTaskClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bfc81b2-6208-4efc-bb35-2bb0a6bfbd34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MathTaskClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void CilinderUnitTest()
+        {
+            double a = 3;
+            double b = 5;
+            double expected = 141.37;
+
+
+            Geometry g = new Geometry();
+            double actual =  g.Cilinder(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
